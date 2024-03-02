808caa3bdabd7579cf29384ea7e8f841feee1e60
HackCEWIT2024 <arhaam.hossain@stonybrook.edu>
2024-03-01 23:59:38 -0500

    config file

---
+++ config.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set pin numbering mode
+GPIO.setmode(GPIO.BCM)
+
+# Define pin numbers
+ground_pin = 6
+buzzer_pin = 12
+blue_led_pin = 16
+green_led_pin = 18
+red_led_pin = 22
+
+# Configure pins
+GPIO.setup(ground_pin, GPIO.OUT)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+GPIO.setup(blue_led_pin, GPIO.OUT)
+GPIO.setup(green_led_pin, GPIO.OUT)
+GPIO.setup(red_led_pin, GPIO.OUT)
+
+try:
+    # Set ground pin to low (ground)
+    GPIO.output(ground_pin, GPIO.LOW)
+
+    # Blink the LEDs and sound the buzzer
+    while True:
+        GPIO.output(buzzer_pin, GPIO.HIGH)
+        GPIO.output(blue_led_pin, GPIO.HIGH)
+        GPIO.output(green_led_pin, GPIO.HIGH)
+        GPIO.output(red_led_pin, GPIO.HIGH)
+        time.sleep(1)
+
+        GPIO.output(buzzer_pin, GPIO.LOW)
+        GPIO.output(blue_led_pin, GPIO.LOW)
+        GPIO.output(green_led_pin, GPIO.LOW)
+        GPIO.output(red_led_pin, GPIO.LOW)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    # Clean up GPIO on exit
+    GPIO.cleanup()
