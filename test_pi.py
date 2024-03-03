0ce8eb9f8e978e9b2b0cfa39bddc6c42a956ccfb
Ryan Lin <Ryan.Lin@stonybrook.edu>
2024-03-02 22:12:32 -0500

    debugging

--- test_pi.py
+++ test_pi.py
@@ -29,20 +29,32 @@ try:
     # Blink the LEDs and sound the buzzer
     while True:
         GPIO.output(buzzer_pin, GPIO.HIGH)
+        print("Buzzer on")
         GPIO.output(blue_led_pin, GPIO.HIGH)
+        print("Blue LED on")
         GPIO.output(green_led_pin, GPIO.HIGH)
+        print("Green LED on")
         GPIO.output(red_led_pin, GPIO.HIGH)
+        print("Red LED on")
         GPIO.output(yellow_led_pin, GPIO.HIGH)
+        print("Yellow LED on")
         GPIO.output(white_led_pin, GPIO.HIGH)
-        time.sleep(1)
+        print("White LED on")
+        time.sleep(5)
 
         GPIO.output(buzzer_pin, GPIO.LOW)
+        print("Buzzer off")
         GPIO.output(blue_led_pin, GPIO.LOW)
+        print("Blue LED off")
         GPIO.output(green_led_pin, GPIO.LOW)
+        print("Green LED off")
         GPIO.output(red_led_pin, GPIO.LOW)
+        print("Red LED off")
         GPIO.output(yellow_led_pin, GPIO.LOW)
+        print("Yellow LED off")
         GPIO.output(white_led_pin, GPIO.LOW)
-        time.sleep(1)
+        print("White LED off")
+        time.sleep(5)
 
 except KeyboardInterrupt:
     # Clean up GPIO on exit
